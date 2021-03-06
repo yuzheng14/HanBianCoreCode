4905bce8f63fb8574b7a81794d0fe6e8a5a037f6
yuzheng14 <422450117@qq.com>
2021-03-06 20:37:37 +0800

    Update TransferTest.java

--- src/test/java/com/yuzheng14/Tools/TransferTest.java
+++ src/test/java/com/yuzheng14/Tools/TransferTest.java
@@ -68,4 +68,15 @@ public class TransferTest {
         Assert.assertEquals("일기",hangulTransfer("읽기"));
         Assert.assertEquals("박따",hangulTransfer("밝다"));
     }
+    @Test
+    public void HangulToPronunciationTest2(){
+        Assert.assertEquals("나는 혼자서 미뢰에 기를 나섣슴미다",hangulTransfer("나는 혼자서  밀회의 길을 나섰습니다"));
+        println("[测试成功]: "+"절대로 고개를 떨구지 말라");
+        Assert.assertEquals("고개를 치켜들고 세상을 똑빠로 바라보라",hangulTransfer("고개를 치켜들고 세상을 똑바로 바라보라"));
+        println("[测试成功]: "+"고개를 치켜들고 세상을 똑바로 바라보라");
+        Assert.assertEquals("자시네 능녀글 실뢰하라",hangulTransfer("자신의 능력을 신뢰하라"));
+        println("[测试成功]: "+"자신의 능력을 신뢰하라");
+        Assert.assertEquals("겸소나지만 함니저긴 자신가멉씨는 성공할 수도 행보칼 수도 업따",hangulTransfer("겸손하지만 합리적인 자신감없이는 성공할 수도 행복할 수도 없다"));
+        println("[测试成功]: "+"겸손하지만 합리적인 자신감없이는 성공할 수도 행복할 수도 없다");
+    }
 }
